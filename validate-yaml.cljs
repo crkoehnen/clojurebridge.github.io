469e166ad6eb05b31cae011bbc5b9c30b2e0bd19
David Chambers <dc@davidchambers.me>
2017-10-19 14:40:23 +0200

    various tweaks and improvements

--- validate-yaml.cljs
+++ validate-yaml.cljs
@@ -1,77 +1,59 @@
-#!/usr/bin/env lumo
-
 (require '[cljs.nodejs :as nodejs])
 (require '[clojure.spec.alpha :as s])
-(require '[clojure.string :as st])
-(def yml (nodejs/require "js-yaml"))
+
 (def fs (nodejs/require "fs"))
+(def js-yaml (nodejs/require "js-yaml"))
 
-(def datetime-regex #"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d [+-]\d\d\d\d")
-(s/def ::date (s/and string? #(re-matches datetime-regex %)))
 
-(def email-regex #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")
-(s/def ::email (s/and string? #(re-matches email-regex %)))
-(s/def ::url (s/and string? #(not (st/blank? %))))
-
-(s/def ::name string?)
-(s/def ::names (s/coll-of ::name))
-(s/def ::organizers (s/keys :req-un [::email ::names]))
-
-(s/def ::layout #(= "post" %))
-
-(s/def ::street string?)
-(s/def ::city string?)
-(s/def ::country string?)
-(s/def ::city_image_url ::url)
-
-(s/def ::workshop_dates string?)
-
-(s/def ::latitude (s/double-in :min -180.0 :max 180.0))
-(s/def ::longitude (s/double-in :min 0 :max 90.0))
-
+(s/def ::url (s/and string? #(not= "" %)))
+(s/def ::email (s/and string? #(.includes % "@")))
 (s/def ::image ::url)
-(s/def ::sponsor (s/keys :req-un [::name ::url ::image]))
-(s/def ::sponsors (s/coll-of ::sponsor))
-
+(s/def ::names (s/coll-of string?))
 (s/def ::service string?)
 (s/def ::username string?)
-(s/def ::account (s/keys :req-un [::service ::username]))
-(s/def ::accounts (s/coll-of ::account))
 
-(s/def ::workshop-metadata (s/keys :req-un [::accounts
-                                            ::city
-                                            ::city_image_url
+(s/def ::layout #(= "post" %))
+(s/def ::country string?)
+(s/def ::city string?)
+(s/def ::street string?)
+(s/def ::latitude (s/double-in :min 0.0 :max 90.0))
+(s/def ::longitude (s/double-in :min -180.0 :max 180.0))
+(s/def ::date (s/and string? #(re-matches #"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d [+-]\d\d\d\d" %)))
+(s/def ::workshop-dates string?)
+(s/def ::city-image-url ::url)
+(s/def ::organizers (s/keys :req-un [::email ::names]))
+(s/def ::accounts (s/coll-of (s/keys :req-un [::service ::username])))
+(s/def ::sponsors (s/coll-of (s/keys :req-un [::name ::url ::image])))
+
+(s/def ::workshop-metadata (s/keys :req-un [::layout
                                             ::country
-                                            ::date
-                                            ::latitude
-                                            ::layout
-                                            ::longitude
-                                            ::organizers
-                                            ::sponsors
+                                            ::city
                                             ::street
-                                            ::workshop_dates]))
+                                            ::latitude
+                                            ::longitude
+                                            ::date
+                                            ::workshop-dates
+                                            ::city-image-url
+                                            ::organizers
+                                            ::accounts
+                                            ::sponsors]))
+
 (defn validate [k o]
   (let [valid (s/valid? k o)]
     (when (not valid) (s/explain k o))
     valid))
 
-(defn read-file [f]
-  (-> (.readFileSync fs f "utf8")
-      (st/split "---\n")
-      second))
+(def validate-file
+     (comp #(validate ::workshop-metadata %)
+           #(js->clj % :keywordize-keys true)
+           #(.safeLoad js-yaml %)
+           second
+           #(clojure.string/split % "---\n")
+           #(.readFileSync fs % "utf8")))
 
-(defn parse-yml [s]
-  (-> (.safeLoad yml s)
-      (js->clj :keywordize-keys true)))
-
-(defn check-file [f]
-  (->> (read-file f)
-       parse-yml
-       (validate ::workshop-metadata)))
-
-(defn read-dir [dir]
-  (let [files (-> (.readdirSync fs dir "utf8") js->clj)
-        valid (every? #(check-file (str dir "/" %)) files)]
+(defn validate-dir [dir]
+  (let [files (js->clj (.readdirSync fs dir "utf8"))
+        valid (every? #(validate-file (str dir "/" %)) files)]
     (js/process.exit (if valid 0 1))))
 
-(read-dir "_posts")
+(validate-dir "_posts")
